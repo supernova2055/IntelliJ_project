808ed4df991a037ed9923b82c392bea7b28a624e
Om Prakash Shah <omprakashshah@Oms-MacBook-Pro.fios-router.home>
2023-12-03 11:51:03 -0500

    First Commit from new Computer

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i < 6; i++){
+            System.out.println("This is car: " + i);
+        }
+    }
+}
