e802b623edf630dacf0c7f5029ba46c307ee33ec
supernova2055 <omprakashinnyc@gmail.com>
2023-12-03 19:25:00 -0500

    Created a new Student Object

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         for(int i = 1; i < 6; i++){
             System.out.println("This is car: " + i);
 
-            Student Harry = new Student();
         }
+
+        Student Student1 = new Student();
+
+        Student1.setAge(25);
+        Student1.setName("Harry");
+
+        System.out.println("Student name: " + Student1.getName());
+        System.out.println("Student age: " + Student1.getAge());
     }
 }
